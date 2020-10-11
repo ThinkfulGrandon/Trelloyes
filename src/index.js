19b5aca61c2995551fecafa3b60fd0ccc596da35
Grandon Smith <grandon.smith@yahoo.com>
2020-10-11 19:23:16 -0400

    ok I'm gonna need help with this

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import STORE from './Store.js'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App store={STORE}/>,
   document.getElementById('root')
 );
 
